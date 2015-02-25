44d1e5cf362f430c8c695a6c16509760ae7cae4d
gabrielkamimura <gabrielk.yano@gmail.com>
2015-02-25 12:58:38 -0300

    Melhoras na forma de evitar interpretação de HTML no documento

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssMinifier = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+    
+gulp.task( 'sass', function() {
+    gulp.src( './src/scss/*.scss' )
+        .pipe( sass() )
+        .pipe( gulp.dest( './src/css' ) );
+    
+} );
+
+gulp.task( 'minify-css', function() {    
+    gulp.src( './src/css/*.css' )
+        .pipe( cssMinifier( { keepBreaks: false } ) )
+        .pipe( gulp.dest( './dist/' ) );
+    
+} );
+ 
+gulp.task('scripts', function() {
+  return gulp.src('./src/componentes/*.js')
+    .pipe(concat('editto.js'))
+    .pipe(gulp.dest('./'));
+});
+ 
+gulp.task('minify-js', function() {
+  gulp.src('./src/editto.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./'))
+});
