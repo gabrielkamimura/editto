e49de7745dadfdd7df3d68e4cb136ae37ce08630
gabrielkamimura <gabrielk.yano@gmail.com>
2016-03-04 18:52:02 -0300

    Permitindo de forma mais fácil remoção de elementos de padrão para caso deseje utilizar apenas personalização

--- src/componentes/editor.js
+++ src/componentes/editor.js
@@ -1,9 +1,19 @@
-function eDitto(textid) {
+/**
+ * [eDitto description]
+ * @param  {string} textid  id do textarea em que vai ser colocado este editor
+ * @param  {object} options  Opções a serem passadas para configurar
+ *   @example  {
+ *             		disableDefaultComponents: true //Devem ser desabilitados os componentes padrão do editor (funcionalidades padão) e só devem ser usadas as personalizações
+ *             }
+ * @return {object}
+ */
+function eDitto(textid, options) {
 
-    var documento = new eDittoDocument(textid, this),
-        botoes = new eDittoButtonBar(documento, textid),
+    var options = options || {};
+        documento = new eDittoDocument(textid, this),
+        botoes = new eDittoButtonBar(documento, textid, options),
         btnAtivacao = [];
-        
+
     //Permitindo edição do documento
     documento.permitirEdicao();
 
@@ -24,176 +34,4 @@ function eDitto(textid) {
     };
 
     var $this = this;
-    /*
-    var btnNegrito = new botao("bold", "Negrito");
-    btnNegrito.getButton().onclick = function() {
-        $this.adicionarBotaoVerificacao(btnNegrito, 'bold');
-        documento.formatar('bold');
-        verificarBotoes();
-    };
-
-    var btnItalico = new botao("italic", "Itálico");
-    btnAtivacao.push({botao: btnItalico, acao: 'italic'});
-    btnItalico.getButton().onclick = function() {
-        documento.formatar('italic');
-        verificarBotoes();
-    };
-
-    var btnUnderline = new botao('underline', "Sublinhado");
-    $this.adicionarBotaoVerificacao(btnUnderline, 'underline');
-    btnUnderline.getButton().onclick = function() {
-        documento.formatar('underline');
-        verificarBotoes();
-    };
-
-    var cores = [
-        {texto: 'Preto', valor: "#000"},
-        {texto: 'Cinza', valor: "#9e9e9e"},
-        {texto: 'Marrom', valor: "#795548"},
-        {texto: 'Azul', valor: "#2196F3"},
-        {texto: 'Vermelho', valor: "#F44336"},
-        {texto: 'Amarelo', valor: "#ffeb3b"},
-        {texto: 'Verde', valor: "#4caf50"},
-        {texto: 'Laranja', valor: "#ff9800"},
-        {texto: 'Roxo', valor: "#9c27b0"},
-        {texto: 'Rosa', valor: "#e91e63"},
-        {texto: 'Ciano', valor: "#00bcd4"},
-        {texto: 'Azul claro', valor: "#03a9f4"},
-        {texto: 'Índigo', valor: "#3f51b5"},
-        {texto: 'Lima', valor: "#cddc39"},
-        {texto: 'Laranja Escuro', valor: "#ff5722"},
-        {texto: 'Rosa Claro', valor: "#F8BBD0"},
-        {texto: 'Teal', valor: "#009688"},
-        {texto: 'Roxo Escuro', valor: "#673AB7"}
-    ];
-    var btnCor = new botao("Fonte",'', 2, cores);
-    btnCor.getButton().onchange = function() {
-        documento.formatar('forecolor', btnCor.getValue());
-    };
-
-
-    var tamanhosTexto = [
-        {texto: '1', valor: '1'},
-        {texto: '2', valor: '2'},
-        {texto: '3', valor: '3'},
-        {texto: '4', valor: '4'},
-        {texto: '5', valor: '5'},
-        {texto: '6', valor: '6'},
-        {texto: '7', valor: '7'}
-    ];
-    var btntamanhofonte = new botao("Fonte",'', 2, tamanhosTexto);
-    btntamanhofonte.getButton().onchange = function() {
-        documento.formatar('fontSize', btntamanhofonte.getValue());
-    };
-
-    var fonts = [{texto: 'Arial', valor: "arial"}, {texto: 'Courier', valor: "courier"}];
-    var btnFont = new botao("Fonte",'', 2, fonts);
-    btnFont.getButton().onchange = function() {
-        documento.formatar('fontname', btnFont.getValue());
-    };
-
-    var btnEsquerda = new botao('align-left', "Alinhar à Esquerda");
-    $this.adicionarBotaoVerificacao(btnEsquerda, 'justifyleft');
-    btnEsquerda.getButton().onclick = function() {
-        documento.formatar('justifyleft');
-        verificarBotoes();
-    };
-
-
-    var btnCentraliza = new botao('align-center', "Centralizar");
-    $this.adicionarBotaoVerificacao(btnCentraliza, 'justifycenter');
-    btnCentraliza.getButton().onclick = function() {
-        documento.formatar('justifycenter');
-        verificarBotoes();
-    };
-
-    var btnDireita = new botao('align-right', "Alinhar à direita");
-    $this.adicionarBotaoVerificacao(btnDireita, 'justifyright');
-    btnDireita.getButton().onclick = function() {
-        documento.formatar('justifyright');
-        verificarBotoes();
-    };
-
-    var btnNum = new botao('list-ol', "Lista Numérica");
-    $this.adicionarBotaoVerificacao(btnNum, 'insertorderedlist');
-    btnNum.getButton().onclick = function() {
-      documento.formatar('insertorderedlist');
-      verificarBotoes();
-    };
-
-    var btnLst = new botao('list-ul', "Lista");
-    $this.adicionarBotaoVerificacao(btnLst, 'insertunorderedlist');
-    btnLst.getButton().onclick = function() {
-      documento.formatar('insertunorderedlist');
-      verificarBotoes();
-    };
-
-    var btnLnk = new botao('link', "link");
-    btnLnk.getButton().onclick = function() {
-        var sLnk = prompt('Digite a URL do link: ', 'http:\/\/');
-        if (sLnk && sLnk != '' && sLnk != 'http://') {
-            documento.formatar('createlink', sLnk);
-        };
-    };
-
-    var btnUlk = new botao('unlink', "Remover Link");
-    btnUlk.getButton().onclick = function() {
-      documento.formatar('unlink');
-    }
-
-    var btnLmp = new botao('eraser', "Limpar Formatação");
-    btnLmp.getButton().onclick = function() {
-      documento.formatar('removeFormat');
-    };
-
-
-
-
- /**
-  * Grupos dos botões. Forma como eles são organizados
-  */
- /*
-   var buttonGroup1 = new grupoBotoes();
-   buttonGroup1.adicionarBotao(btnFont);
-   buttonGroup1.adicionarBotao(btnCor);
-   buttonGroup1.adicionarBotao(btntamanhofonte);
-
-   var buttonGroup2 = new grupoBotoes();
-   buttonGroup2.adicionarBotao(btnNegrito);
-   buttonGroup2.adicionarBotao(btnItalico);
-   buttonGroup2.adicionarBotao(btnUnderline);
-
-   var buttonGroup3 = new grupoBotoes();
-   buttonGroup3.adicionarBotao(btnEsquerda);
-   buttonGroup3.adicionarBotao(btnCentraliza);
-   buttonGroup3.adicionarBotao(btnDireita);
-
-   var buttonGroup4 = new grupoBotoes();
-   buttonGroup4.adicionarBotao(btnNum);
-   buttonGroup4.adicionarBotao(btnLst);
-
-   var buttonGroup5 = new grupoBotoes();
-   buttonGroup5.adicionarBotao(btnLnk);
-   buttonGroup5.adicionarBotao(btnUlk);
-   buttonGroup5.adicionarBotao(btnLmp);
-
-   botoes.adicionarGrupo(buttonGroup1.getGroup());
-   botoes.adicionarGrupo(buttonGroup2.getGroup());
-   botoes.adicionarGrupo(buttonGroup3.getGroup());
-   botoes.adicionarGrupo(buttonGroup4.getGroup());
-   botoes.adicionarGrupo(buttonGroup5.getGroup());
-
-
-   /*
-      Esta parte é para a definição das personalizações
-   */
-   /*
-   var btnGrpCst = new eDittoButtonGroup(botoes);
-console.log(btnGrpCst);
-   this.addPersonalizacao = function(personalizacao) {
-        btnGrpCst.adicionarBotao(personalizacao.ac.btn);
-   };
-
-   botoes.adicionarGrupo(btnGrpCst.getGroup());
-   */
 }
--- src/editto.js
+++ src/editto.js
@@ -1,17 +1,14 @@
 /**
- * @deprecated Depreciado para alterações futuras de melhoria de versativização
- * @todo -> integrar funções ao eDittoDocument (documentoEditor)
+ * [eDittoButtonBar description]
+ * @param  {object} documento
+ * @param  {string} textid    id para div de barra de botões
+ * @param  {object} options   Opções a serem passadas para configurar (passadas de eDitto)
+ *   @example  {
+ *             		disableDefaultComponents: true //Devem ser desabilitados os componentes padrão do editor (funcionalidades padão e só devem ser usadas as personalizações)
+ *             }
+ * @return {[type]}           [description]
  */
-function acao(documento) {
-
-  
-
-    //this.btn = new eDittoButton();
-
-
-}
-
-function eDittoButtonBar(documento, textid) {
+function eDittoButtonBar(documento, textid, options) {
 
     var btnAtivacao = []; //Array dos botões a terem as ações monitoradass para a marcação do botão
     var documento = documento;
@@ -59,6 +56,137 @@ function eDittoButtonBar(documento, textid) {
     });
 
 
+
+    if (!options.disableDefaultComponents) {
+      var $this = this;
+      console.log("Carregando elementos padrão do editor");
+      var buttonGroup1 = new eDittoButtonGroup($this);
+
+      var btnNegrito = new eDittoButton(buttonGroup1, "bold", "Negrito");
+      btnNegrito.getButtonDOM().onclick = function() {
+          $this.adicionarBotaoVerificacao(btnNegrito, 'bold');
+          documento.formatar('bold');
+          $this.verificarBotoes();
+      };
+
+      var btnItalico = new eDittoButton(buttonGroup1, "italic", "Itálico");
+      $this.adicionarBotaoVerificacao(btnItalico, 'italic');
+      btnItalico.getButtonDOM().onclick = function() {
+          documento.formatar('italic');
+          $this.verificarBotoes();
+      };
+
+      var btnUnderline = new eDittoButton(buttonGroup1, 'underline', "Sublinhado");
+      $this.adicionarBotaoVerificacao(btnUnderline, 'underline');
+      btnUnderline.getButtonDOM().onclick = function() {
+          documento.formatar('underline');
+          $this.verificarBotoes();
+      };
+
+      var buttonGroup2 = new eDittoButtonGroup($this);
+      var cores = [
+          {texto: 'Preto', valor: "#000"},
+          {texto: 'Cinza', valor: "#9e9e9e"},
+          {texto: 'Marrom', valor: "#795548"},
+          {texto: 'Azul', valor: "#2196F3"},
+          {texto: 'Vermelho', valor: "#F44336"},
+          {texto: 'Amarelo', valor: "#ffeb3b"},
+          {texto: 'Verde', valor: "#4caf50"},
+          {texto: 'Laranja', valor: "#ff9800"},
+          {texto: 'Roxo', valor: "#9c27b0"},
+          {texto: 'Rosa', valor: "#e91e63"},
+          {texto: 'Ciano', valor: "#00bcd4"},
+          {texto: 'Azul claro', valor: "#03a9f4"},
+          {texto: 'Índigo', valor: "#3f51b5"},
+          {texto: 'Lima', valor: "#cddc39"},
+          {texto: 'Laranja Escuro', valor: "#ff5722"},
+          {texto: 'Rosa Claro', valor: "#F8BBD0"},
+          {texto: 'Teal', valor: "#009688"},
+          {texto: 'Roxo Escuro', valor: "#673AB7"}
+      ];
+      var btnCor = new eDittoButton(buttonGroup2, "Fonte",'', 2, cores);
+      btnCor.getButtonDOM().onchange = function() {
+          documento.formatar('forecolor', btnCor.getValue());
+      };
+
+
+      var tamanhosTexto = [
+          {texto: '1', valor: '1'},
+          {texto: '2', valor: '2'},
+          {texto: '3', valor: '3'},
+          {texto: '4', valor: '4'},
+          {texto: '5', valor: '5'},
+          {texto: '6', valor: '6'},
+          {texto: '7', valor: '7'}
+      ];
+      var btntamanhofonte = new eDittoButton(buttonGroup2, "Fonte",'', 2, tamanhosTexto);
+      btntamanhofonte.getButtonDOM().onchange = function() {
+          documento.formatar('fontSize', btntamanhofonte.getValue());
+      };
+
+      var fonts = [{texto: 'Arial', valor: "arial"}, {texto: 'Courier', valor: "courier"}];
+      var btnFont = new eDittoButton(buttonGroup2, "Fonte",'', 2, fonts);
+      btnFont.getButtonDOM().onchange = function() {
+          documento.formatar('fontname', btnFont.getValue());
+      };
+
+      var buttonGroup3 = new eDittoButtonGroup($this);
+      var btnEsquerda = new eDittoButton(buttonGroup3, 'align-left', "Alinhar à Esquerda");
+      $this.adicionarBotaoVerificacao(btnEsquerda, 'justifyleft');
+      btnEsquerda.getButtonDOM().onclick = function() {
+          documento.formatar('justifyleft');
+          $this.verificarBotoes();
+      };
+
+
+      var btnCentraliza = new eDittoButton(buttonGroup3, 'align-center', "Centralizar");
+      $this.adicionarBotaoVerificacao(btnCentraliza, 'justifycenter');
+      btnCentraliza.getButtonDOM().onclick = function() {
+          documento.formatar('justifycenter');
+          $this.verificarBotoes();
+      };
+
+      var btnDireita = new eDittoButton(buttonGroup3, 'align-right', "Alinhar à direita");
+      $this.adicionarBotaoVerificacao(btnDireita, 'justifyright');
+      btnDireita.getButtonDOM().onclick = function() {
+          documento.formatar('justifyright');
+          $this.verificarBotoes();
+      };
+
+      var buttonGroup4 = new eDittoButtonGroup($this);
+      var btnNum = new eDittoButton(buttonGroup4, 'list-ol', "Lista Numérica");
+      $this.adicionarBotaoVerificacao(btnNum, 'insertorderedlist');
+      btnNum.getButtonDOM().onclick = function() {
+        documento.formatar('insertorderedlist');
+        $this.verificarBotoes();
+      };
+
+      var btnLst = new eDittoButton(buttonGroup4, 'list-ul', "Lista");
+      $this.adicionarBotaoVerificacao(btnLst, 'insertunorderedlist');
+      btnLst.getButtonDOM().onclick = function() {
+        documento.formatar('insertunorderedlist');
+        $this.verificarBotoes();
+      };
+
+      var buttonGroup5 = new eDittoButtonGroup($this);
+      var btnLnk = new eDittoButton(buttonGroup5, 'link', "link");
+      btnLnk.getButtonDOM().onclick = function() {
+          var sLnk = prompt('Digite a URL do link: ', 'http:\/\/');
+          if (sLnk && sLnk != '' && sLnk != 'http://') {
+              documento.formatar('createlink', sLnk);
+          };
+      };
+
+      var btnUlk = new eDittoButton(buttonGroup5, 'unlink', "Remover Link");
+      btnUlk.getButtonDOM().onclick = function() {
+        documento.formatar('unlink');
+      }
+
+      var btnLmp = new eDittoButton(buttonGroup5, 'eraser', "Limpar Formatação");
+      btnLmp.getButtonDOM().onclick = function() {
+        documento.formatar('removeFormat');
+      };
+    }
 }
 
 /**
@@ -406,12 +534,22 @@ function eDittoDocument(textid, editor) {
 
 }
 
-function eDitto(textid) {
+/**
+ * [eDitto description]
+ * @param  {string} textid  id do textarea em que vai ser colocado este editor
+ * @param  {object} options  Opções a serem passadas para configurar
+ *   @example  {
+ *             		disableDefaultComponents: true //Devem ser desabilitados os componentes padrão do editor (funcionalidades padão) e só devem ser usadas as personalizações
+ *             }
+ * @return {object}
+ */
+function eDitto(textid, options) {
 
-    var documento = new eDittoDocument(textid, this),
-        botoes = new eDittoButtonBar(documento, textid),
+    var options = options || {};
+        documento = new eDittoDocument(textid, this),
+        botoes = new eDittoButtonBar(documento, textid, options),
         btnAtivacao = [];
-        
+
     //Permitindo edição do documento
     documento.permitirEdicao();
 
@@ -432,178 +570,6 @@ function eDitto(textid) {
     };
 
     var $this = this;
-    /*
-    var btnNegrito = new botao("bold", "Negrito");
-    btnNegrito.getButton().onclick = function() {
-        $this.adicionarBotaoVerificacao(btnNegrito, 'bold');
-        documento.formatar('bold');
-        verificarBotoes();
-    };
-
-    var btnItalico = new botao("italic", "Itálico");
-    btnAtivacao.push({botao: btnItalico, acao: 'italic'});
-    btnItalico.getButton().onclick = function() {
-        documento.formatar('italic');
-        verificarBotoes();
-    };
-
-    var btnUnderline = new botao('underline', "Sublinhado");
-    $this.adicionarBotaoVerificacao(btnUnderline, 'underline');
-    btnUnderline.getButton().onclick = function() {
-        documento.formatar('underline');
-        verificarBotoes();
-    };
-
-    var cores = [
-        {texto: 'Preto', valor: "#000"},
-        {texto: 'Cinza', valor: "#9e9e9e"},
-        {texto: 'Marrom', valor: "#795548"},
-        {texto: 'Azul', valor: "#2196F3"},
-        {texto: 'Vermelho', valor: "#F44336"},
-        {texto: 'Amarelo', valor: "#ffeb3b"},
-        {texto: 'Verde', valor: "#4caf50"},
-        {texto: 'Laranja', valor: "#ff9800"},
-        {texto: 'Roxo', valor: "#9c27b0"},
-        {texto: 'Rosa', valor: "#e91e63"},
-        {texto: 'Ciano', valor: "#00bcd4"},
-        {texto: 'Azul claro', valor: "#03a9f4"},
-        {texto: 'Índigo', valor: "#3f51b5"},
-        {texto: 'Lima', valor: "#cddc39"},
-        {texto: 'Laranja Escuro', valor: "#ff5722"},
-        {texto: 'Rosa Claro', valor: "#F8BBD0"},
-        {texto: 'Teal', valor: "#009688"},
-        {texto: 'Roxo Escuro', valor: "#673AB7"}
-    ];
-    var btnCor = new botao("Fonte",'', 2, cores);
-    btnCor.getButton().onchange = function() {
-        documento.formatar('forecolor', btnCor.getValue());
-    };
-
-
-    var tamanhosTexto = [
-        {texto: '1', valor: '1'},
-        {texto: '2', valor: '2'},
-        {texto: '3', valor: '3'},
-        {texto: '4', valor: '4'},
-        {texto: '5', valor: '5'},
-        {texto: '6', valor: '6'},
-        {texto: '7', valor: '7'}
-    ];
-    var btntamanhofonte = new botao("Fonte",'', 2, tamanhosTexto);
-    btntamanhofonte.getButton().onchange = function() {
-        documento.formatar('fontSize', btntamanhofonte.getValue());
-    };
-
-    var fonts = [{texto: 'Arial', valor: "arial"}, {texto: 'Courier', valor: "courier"}];
-    var btnFont = new botao("Fonte",'', 2, fonts);
-    btnFont.getButton().onchange = function() {
-        documento.formatar('fontname', btnFont.getValue());
-    };
-
-    var btnEsquerda = new botao('align-left', "Alinhar à Esquerda");
-    $this.adicionarBotaoVerificacao(btnEsquerda, 'justifyleft');
-    btnEsquerda.getButton().onclick = function() {
-        documento.formatar('justifyleft');
-        verificarBotoes();
-    };
-
-
-    var btnCentraliza = new botao('align-center', "Centralizar");
-    $this.adicionarBotaoVerificacao(btnCentraliza, 'justifycenter');
-    btnCentraliza.getButton().onclick = function() {
-        documento.formatar('justifycenter');
-        verificarBotoes();
-    };
-
-    var btnDireita = new botao('align-right', "Alinhar à direita");
-    $this.adicionarBotaoVerificacao(btnDireita, 'justifyright');
-    btnDireita.getButton().onclick = function() {
-        documento.formatar('justifyright');
-        verificarBotoes();
-    };
-
-    var btnNum = new botao('list-ol', "Lista Numérica");
-    $this.adicionarBotaoVerificacao(btnNum, 'insertorderedlist');
-    btnNum.getButton().onclick = function() {
-      documento.formatar('insertorderedlist');
-      verificarBotoes();
-    };
-
-    var btnLst = new botao('list-ul', "Lista");
-    $this.adicionarBotaoVerificacao(btnLst, 'insertunorderedlist');
-    btnLst.getButton().onclick = function() {
-      documento.formatar('insertunorderedlist');
-      verificarBotoes();
-    };
-
-    var btnLnk = new botao('link', "link");
-    btnLnk.getButton().onclick = function() {
-        var sLnk = prompt('Digite a URL do link: ', 'http:\/\/');
-        if (sLnk && sLnk != '' && sLnk != 'http://') {
-            documento.formatar('createlink', sLnk);
-        };
-    };
-
-    var btnUlk = new botao('unlink', "Remover Link");
-    btnUlk.getButton().onclick = function() {
-      documento.formatar('unlink');
-    }
-
-    var btnLmp = new botao('eraser', "Limpar Formatação");
-    btnLmp.getButton().onclick = function() {
-      documento.formatar('removeFormat');
-    };
-
-
-
-
- /**
-  * Grupos dos botões. Forma como eles são organizados
-  */
- /*
-   var buttonGroup1 = new grupoBotoes();
-   buttonGroup1.adicionarBotao(btnFont);
-   buttonGroup1.adicionarBotao(btnCor);
-   buttonGroup1.adicionarBotao(btntamanhofonte);
-
-   var buttonGroup2 = new grupoBotoes();
-   buttonGroup2.adicionarBotao(btnNegrito);
-   buttonGroup2.adicionarBotao(btnItalico);
-   buttonGroup2.adicionarBotao(btnUnderline);
-
-   var buttonGroup3 = new grupoBotoes();
-   buttonGroup3.adicionarBotao(btnEsquerda);
-   buttonGroup3.adicionarBotao(btnCentraliza);
-   buttonGroup3.adicionarBotao(btnDireita);
-
-   var buttonGroup4 = new grupoBotoes();
-   buttonGroup4.adicionarBotao(btnNum);
-   buttonGroup4.adicionarBotao(btnLst);
-
-   var buttonGroup5 = new grupoBotoes();
-   buttonGroup5.adicionarBotao(btnLnk);
-   buttonGroup5.adicionarBotao(btnUlk);
-   buttonGroup5.adicionarBotao(btnLmp);
-
-   botoes.adicionarGrupo(buttonGroup1.getGroup());
-   botoes.adicionarGrupo(buttonGroup2.getGroup());
-   botoes.adicionarGrupo(buttonGroup3.getGroup());
-   botoes.adicionarGrupo(buttonGroup4.getGroup());
-   botoes.adicionarGrupo(buttonGroup5.getGroup());
-
-
-   /*
-      Esta parte é para a definição das personalizações
-   */
-   /*
-   var btnGrpCst = new eDittoButtonGroup(botoes);
-console.log(btnGrpCst);
-   this.addPersonalizacao = function(personalizacao) {
-        btnGrpCst.adicionarBotao(personalizacao.ac.btn);
-   };
-
-   botoes.adicionarGrupo(btnGrpCst.getGroup());
-   */
 }
 
 function eDittoButtonGroup(barraBotoes) {
@@ -640,17 +606,4 @@ function eDittoButtonGroup(barraBotoes) {
 
     // Adicionando grupo a barra de botões
     barraBotoes.adicionarGrupo(this);
-}
-
-/**
- * @deprecated Depreciado para alterações futuras de melhoria de versativização
- * @todo -> integrar funcionalidades ao botão
- */
-function personalizacaoEditor( documento ) {
-
-    this.ac = new acao( documento );
-
-     this.definirIcone = function(icone) {
-        this.ac.btn.definirIcone(icone);
-    };
 }
