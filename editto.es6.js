530b47169cd4e54aede75171527c1c297584b201
Gabriel <gabrielk.yano@gmail.com>
2018-04-13 17:42:35 -0300

    Formatações do editto

--- editto.es6.js
+++ editto.es6.js
@@ -1,57 +1,3 @@
-class eDittoButton extends HTMLElement {
-    static get observedAttributes() {return ['w', 'l']; }
-    
-    constructor(textarea, buttonBarTemplate) {
-        super();
-        this.shadow = this.createShadowRoot();
-        this.buttomDOM = document.createElement('button');
-        this.buttomDOM.innerHTML = "Botão"
-        var style = document.createElement('style');
-        this.shadow.appendChild(style);
-        this.shadow.appendChild(this.buttomDOM);
-        this.eDitto = this.parentNode.parentNode.parentNode;
-        
-        this.buttomDOM.onclick = function() {
-            console.log("Clicou", this.eDitto);
-        }
-    }
-    
-    connectedCallback() {
-      }
-
-      disconnectedCallback() {
-      }
-
-      adoptedCallback() {
-      }
-}
-
-customElements.define('editto-button', eDittoButton);
-
-class eDittoButtonGroup extends HTMLElement {
-    static get observedAttributes() {return ['w', 'l']; }
-    
-    constructor(textarea, buttonBarTemplate) {
-        super();
-        this.shadow = this.createShadowRoot();
-        this.div = document.createElement('div');
-        var style = document.createElement('style');
-        this.shadow.appendChild(style);
-        this.shadow.appendChild(this.div);
-    }
-    
-    connectedCallback() {
-      }
-
-      disconnectedCallback() {
-      }
-
-      adoptedCallback() {
-      }
-}
-
-customElements.define('editto-button-group', eDittoButtonGroup);
-
 class eDitto extends HTMLElement {
     static get observedAttributes() {return ['w', 'l']; }
     
@@ -73,6 +19,7 @@ class eDitto extends HTMLElement {
                 console.log("Foi")
             }
         }
+      this.allowEdition();
     }
     
     connectedCallback() {
@@ -87,8 +34,30 @@ class eDitto extends HTMLElement {
         console.log('Custom element moved to new page.');
       }
     
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        console.log("Atribute change")
+        if (attrName == 'value') {
+        }
+    }
+    
     get iframeDocument() {
-        
+        if (this.iframe) {
+            return this.iframe.contentWindow.document;
+
+        } else {
+        // se contentDocument existe
+          if (this.iframe.contentDocument) {
+            return this.iframe.contentDocument;
+          } else {
+            if (this.iframe.document) {
+                // IE
+                return this.iframe.document;
+            }
+            else {
+                return null;
+            };
+          }
+        }
     }
     
     // Buttons and button groups will be inserted as HTML templates
@@ -97,32 +66,70 @@ class eDitto extends HTMLElement {
         
     }
     
-    getValue() {
-        
+    get value() {
+        return this.innerHTML = this.iframeDocument.body.innerHTML
     }
     
-    setValue(valor) {
-        
+    set value(newValue) {
+        this.iframeDocument.body.innerHTML = newValue;
     }
     
-    getSelection() {
-        
+    get selection() {
+        let txt = null;
+        if (this.iframeDocument.getSelection) {
+            txt = this.iframeDocument.getSelection();
+        } else if (this.iframeDocument.getSelection()) {
+            txt = this.iframeDocument.getSelection();
+        } else if (this.iframeDocument.selection) {
+            txt = this.iframeDocument.selection.createRange().text;
+        }
+        return txt;
     }
     
-    getSelectionRange() {
-        
+    get selectionRange() {
+        let range = null;
+        if (this.selection.getRangeAt) {
+          range = this.selection.getRangeAt(0);
+        } else {
+          range = document.createRange();
+          range.setStart (userSelection.anchorNode, userSelection.anchorOffset);
+          range.setEnd (userSelection.focusNode, userSelection.focusOffset);
+        }
+
+        return range;
     }
     
-    getSelectedText() {
+    get selectedText() {
+        let textoSelecionado = null;
+        try {
+            textoSelecionado = this.selection.toString();
+        } 
+        catch(error) {
         
+        }
+
+        return textoSelecionado.length > 0 ? textoSelecionado : null;
     }
     
-    getSelectedHTML() {
+    get selectedHTML() {
+        let elementoSelecionado = null;
+        try {
+            let range = this.selectionRange.cloneContents(),
+                tmp = document.createElement('div');
+                tmp.appendChild(range);
+
+            elementoSelecionado = tmp.innerHTML;
+        } 
+        catch(error) {
         
+        }
+
+        return elementoSelecionado.length > 0 ? elementoSelecionado : null;
     }
     
     allowEdition() {
-        
+        this.syncFromsyncFromInnerHTML();
+        this.iframeDocument.designMode = 'On';
     }
     
     disallowEdition() {
@@ -133,8 +140,8 @@ class eDitto extends HTMLElement {
         
     }
     
-    syncFromTextarea() {
-        
+    syncFromsyncFromInnerHTML() {
+        this.value = this.innerHTML;
     }
     
     startAutoSync() {
@@ -145,43 +152,127 @@ class eDitto extends HTMLElement {
         
     }
     
-    format(formato, valor) {
-        console.log(this)
+    format(name, value = null) {
+        this.iframe.focus();
+        this.iframeDocument.execCommand(name, false, value);
+        this.iframe.focus();
     }
     
-    insertElement(elem) {
-        
+    insertElement(elem, options) {
+        if (this.selection) {
+            let a = this.iframeDocument.createElement('div');
+            let range = this.selectionRange;
+            range.surroundContents(a);
+            if (options) {
+                for (let i in options) {
+                    let aux = "{{ " + i + " }}";
+                    elem = eDittoHelpers.replaceAll(elem, aux, eDittoHelpers.escapeHTML(options[i]));
+                };
+            }
+            a.innerHTML = elem;
+        };
     }
     
     insertHTML(htmlText, options) {
-        
+        let text = txt + '<br/>'; //A quebra de linha é para evitar que o documento após a personalização fique inalterável
+        this.insertElement(text, options);
     }
     
-    insertText(text, options) {
-        
+    insertText(text) {
+        this.insertElement(eDittoHelpers.escapeHTML(text));
     }
     
     insertFromTemplate(template, options) {
-        
+        let xhttp = new XMLHttpRequest();
+        let $this = this;
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+
+                let text = this.responseText + '<br/>';
+
+                $this.insertElement(text, options);
+            }   
+        };
+
+        xhttp.open("GET", template, true);
+        xhttp.send();
     }
     
     checkFormat(format) {
         
     }
     
-    setBold() {
-        
+    toggleBold() {
+        this.format('bold');
     }
     
-    setItalic() {
-        
+    toggleItalic() {
+        this.format('italic');
     }
     
-    setUnderline() {
-        
+    toggleUnderline() {
+        this.format('underline');
     }
     
     
 }
 
 customElements.define('editto-editor', eDitto);
+
+/**
+ * Funcionalidade gerais a serem utilizadas
+ */
+window.eDittoHelpers = {
+    /**
+     * Insere um elemtno anteriormente a outro. Utilizado para inserir componentes como a barra de botões e o documento
+     */
+    insertAfter: function(newElement,targetElement) {
+        var parent = targetElement.parentNode;
+
+        //if the parents lastchild is the targetElement...
+        if (parent.lastchild == targetElement) {
+            //add the newElement after the target element.
+            parent.appendChild(newElement);
+        } else {
+            // else the target has siblings, insert the new element between the target and it's next sibling.
+            parent.insertBefore(newElement, targetElement.nextSibling);
+        }
+    },
+
+    /**
+     * Obtém a localização da pasta do arquivo do eDitto(editto.js ou editto.min.js)
+     */
+    getFileLocation: function() {
+        var scriptElements = document.getElementsByTagName('script');
+        for (var i = 0; i < scriptElements.length; i++) {
+            var source = scriptElements[i].src;
+            var fileIndex = source.indexOf('editto.min.js') > -1 ? source.indexOf('editto.min.js') : source.indexOf('editto.js');
+            if (fileIndex > -1) {
+                var location = source.substring(0, fileIndex);
+                return location.substring(location.indexOf('dist'), 0);
+            }
+        }
+      return false;
+    },
+    
+    /**
+     * Recebe uma string e um determinado caractere a ser alterado e retorna string com caracteres alterados
+     * @param  {string} string   A string a sofrer alteração
+     * @param  {string} token    Texto a ser buscado na string para modufucação
+     * @param  {string} newtoken Texto a reposicionar token
+     * @return {string}
+     */
+    replaceAll: function(string, token, newtoken) {
+        var string = string || "";
+        while (string.indexOf(token) != -1) {
+          string = string.replace(token, newtoken);
+        }
+        return string;
+    },
+        
+    escapeHTML: function(string = '') {
+        string = this.replaceAll(this.replaceAll(string, "<", "&lt"), ">", "&gt");
+        return string;
+    }
+}
