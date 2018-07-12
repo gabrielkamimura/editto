23c800783efe91f0d4fa2ab0f6e65dbdfc8ade01
Gabriel <gabrielk.yano@gmail.com>
2018-07-12 12:03:00 -0300

    Melhorias de componentes

--- components-example/questao-academico.es6.js
+++ components-example/questao-academico.es6.js
@@ -23,28 +23,31 @@ class AcademicoQuestao extends HTMLElement {
         let shadow = this.attachShadow({mode: 'open'});
         
         this._updateView = () => {
-            
-            QuestaoService.getById(this.idquestao)
-                .then((questao) => {
-                    this._questao = questao;
-                    shadow.innerHTML = this._template();
-                    if (!shadow.querySelector) {
-                        shadow.querySelector = shadow.host.querySelector;
-                    }
-                let domAlternatives = shadow.querySelectorAll('.question__alternative');
-                let $this = this;
-                for (let i in domAlternatives) {
-                    if (typeof domAlternatives[i] == 'object') {
-                        domAlternatives[i].addEventListener('click', function(event) {
-                            var idAlternativa = this.dataset.idAlternativa;
-                            $this.responderQuestao(idAlternativa);
-                        });
-                    }
+            shadow.innerHTML = this._template();
+            if (!shadow.querySelector) {
+                shadow.querySelector = shadow.host.querySelector;
+            }
+            let domAlternatives = shadow.querySelectorAll('.question__alternative');
+            let $this = this;
+            for (let i in domAlternatives) {
+                if (typeof domAlternatives[i] == 'object') {
+                    domAlternatives[i].addEventListener('click', function(event) {
+                        var idAlternativa = this.dataset.idAlternativa;
+                        $this.responderQuestao(idAlternativa);
+                    });
                 }
-                }).catch((error) => {
-                    console.log(error);
-                })
+            }
         };
+    }
+    
+    carregarQuestao() {
+        QuestaoService.getById(this.idquestao)
+            .then((questao) => {
+                this._questao = questao;
+                this._updateView();
+            }).catch((error) => {
+                console.log(error);
+            });
     }
     
     responderQuestao(idAlternativa) {
@@ -74,68 +77,107 @@ class AcademicoQuestao extends HTMLElement {
     _template() {
         return `
                 <style>
-                    .question__container {
+                    .question__container 
+                    {
                         margin: 1em;
                         color: #4A4A4A;
                         margin-top: 4em;
                     }
-                    hr {
+
+                    hr 
+                    {
                         height: 10px; 
                         width: 100%;
                         margin-bottom: 0;
-                        background-color: #55EFC4;
+                        background-color: #10ac84;
+                        background-image: linear-gradient(to right, #868f96 0%, #596164 100%);
                         border: none;
                     }
-                    .question__subheader {
+
+                    hr.correct {
+                      background-image: linear-gradient(-20deg, #8ddad5 0%, #00cdac 100%);
+                    }
+                    
+                    hr.incorrect {
+                      background-image: linear-gradient(-20deg, #FF5300 0%, #d63031 100%);
+                    }
+
+                    .question__subheader 
+                    {
                         margin: 0;
                         font-size: 16px;
                         font-weight: bold;
                     }
-                    .question__title {
+
+                    .question__title 
+                    {
                         font-size: 16px;
                         margin-top: 0;
                         font-weight: 100;
                     }
                     
-                    .question__alternatives {
+                    .question__alternatives 
+                    {
                         list-style-type: lower-alpha;
                         font-weight: bold;
                     }
                 
-                    .question__alternative {
+                    .question__alternative 
+                    {
                         font-size: 16px;
-                        padding: .5em;
+                        padding: 1em;
                         border: 3px solid rgba(0,0,0,0);
+                        border-left: none;
+                        border-right: none;
                     }
 
-                    .question__alternative--text {
+                    .question__alternative--text 
+                    {
                         font-weight: normal;
                     }
 
-                    .question__alternative:hover:not(.question-alternativa--selected) {
-                        color: #55EFC4;
+                    .question__alternative:hover:not(.question-alternativa--selected) 
+                    {
                         cursor: pointer;
-                        border: 3px solid #55EFC4;
+                        background-color: #c8d6e5;
                     }
-                    .question__alternative:hover:not(.question-alternativa--selected) .question__alternative--text {
+
+                    .question__alternative:hover:not(.question-alternativa--selected) 
+                    .question__alternative--text 
+                    {
                         color: #4A4A4A;
                     }
 
                     .question-alternativa--selected {
-                        border: 3px solid #d63031;
+                        background-color: #d63031;
+                        background-image: linear-gradient(-20deg, #FF5300 0%, #d63031 100%);
                     }
-                    li.question-alternativa--selected.question-alternativa--correct {
-                        border-color: #00b894;
+
+                    .question-alternativa--selected
+                    .question__alternative--text
+                    {
+                        color: #fff;
+                    }       
+                    
+                    li.question-alternativa--selected.question-alternativa--correct 
+                    {
+                        background-color: #10ac84;
+                        background-image: linear-gradient(-20deg, #8ddad5 0%, #00cdac 100%);
+                    }
+
+                    .question-msg {
+                        color: #fff;
                     }
                 </style>
                 <section class="question__container">
-                    <hr />
+                    <hr class="${this._alternativaAtual ? this.isCorreta ? 'correct' : 'incorrect' : ''}"/>
                     <h1 class="question__subheader">Questão</h1>
                     <p class="question__title">${this._questao.texto}</p>
                     <ol class="question__alternatives">
                         ${this._questao.alternativas.map(a => `
                             <li data-id-alternativa="${a.id}" class="question__alternative ${a.id == this._alternativaAtual ? 'question-alternativa--selected' : ''} ${this.isCorreta ? 'question-alternativa--correct' : ''}">
-                                <span class="question__alternative--text">${a.texto}</span>    
+                                <span class="question__alternative--text">${a.texto}</span>
+                                ${a.id == this._alternativaAtual ? this.isCorreta ? '<div class="question-msg">Parabéns, você acertou :)</div>' : '<div class="question-msg">Desculpe, resposta errada. Tente novamente</div>' : ''}
                             </li>
                         `).join('')}
                     </ol>
@@ -144,7 +186,7 @@ class AcademicoQuestao extends HTMLElement {
     
     attributeChangedCallback(attrName, oldVal, newVal) {
         if (attrName == 'idquestao') {
-            this._updateView();
+            this.carregarQuestao();
         }
     }
 }
